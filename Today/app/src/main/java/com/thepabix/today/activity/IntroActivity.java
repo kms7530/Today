36fa4ab1399557dc950d299a38661ac0ad6be025
Kwak Minseok <kms7530@gmail.com>
2015-08-09 14:00:04 +0900

    인트로 액티비티 작업

---
+++ Today/app/src/main/java/com/thepabix/today/activity/IntroActivity.java
@@ -0,0 +1,39 @@
+package com.thepabix.today.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.thepabix.today.R;
+
+public class IntroActivity extends Activity {
+    private SharedPreferences pref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        pref = getSharedPreferences("setting", MODE_PRIVATE);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent;
+                boolean isFirstTime = pref.getBoolean("isFirstTime", true);
+
+                if(isFirstTime) {
+                    intent = new Intent(IntroActivity.this, MainActivity.class);
+                }
+                else {
+                    intent = new Intent(IntroActivity.this, MainActivity.class);
+                }
+
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
